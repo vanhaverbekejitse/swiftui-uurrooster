60ed32ff65132eb61dd07757572d3a2cec070a8b
vanhaverbekejitse <jitse.vanhaverbeke@student.vives.be>
2024-02-05 14:55:24 +0100

    first commit

---
+++ SwiftUI-Uurrooster/SwiftUI_UurroosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UurroosterApp.swift
+//  SwiftUI-Uurrooster
+//
+//  Created by docent on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UurroosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
